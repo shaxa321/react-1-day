3233d73b44e74fbc676149b1866536087db6c5fc
shaxa321 <amonovshahruz@gmail.com>
2023-02-06 16:13:06 +0000

    finito

--- myfirstreactapp/src/ImageComponent.jsx
+++ myfirstreactapp/src/ImageComponent.jsx
@@ -1,6 +1,6 @@
 import { Component } from "react";
 
-class ImageComponent extends Component() {
+class ImageComponent extends Component {
   render() {
     console.log(this.props.sorgente);
     return <img src={this.props.sorgente} alt="ciao" />;
